f6e45f39ff16a53e01d1e0b0d14dd39d8f88bc5d
Chris Davis <chris.davis5440@gmail.com>
2018-10-01 15:33:04 -0400

    created add article and get articles routes

--- routes/articles.js
+++ routes/articles.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const Article = require("../models/article");
 
 //GET Articles
 router.get("/", (req, res) => {
-  res.render("articles", {
-    title: "Articles",
-    flash: { success: req.flash("success") }
+  Article.getArticles((err, articles) => {
+    if(err) {
+      res.send(err);
+    }
+
+    res.render("articles", {
+      title: "Articles",
+      articles: articles,
+      flash: { success: req.flash("success") }
+    });
   });
 });
 
@@ -22,6 +30,24 @@ router.get("/category/:category_id", (req, res) => {
   res.render("articles", {
     title: "Category Articles",
     flash: { success: req.flash("success") }
+  });
+});
+
+//POST Add Article
+router.post("/add", (req, res) => {
+  let article = new Article();
+  article.title = req.body.title;
+  article.subtitle = req.body.subtitle;
+  article.category = req.body.category;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  Article.addArticle(article, (err, article) => {
+    if (err) {
+      res.send(err);
+    }
+
+    res.redirect("/manage/articles");
   });
 });
 
